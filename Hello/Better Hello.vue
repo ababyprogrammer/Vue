398164970a13c0382ab94d6fffce85e6cc26a199
A baby programmer <139476654+ababyprogrammer@users.noreply.github.com>
2023-11-27 11:16:23 +0330

    Add files via upload

---
+++ Hello/Better Hello.vue
@@ -0,0 +1,27 @@
+<template lang="jade">
+    div
+    p {{ greeting }} World!
+    other-component
+</template>
+
+<script>
+import OtherComponent from "./OtherComponent.vue";
+
+export default {
+    data() {
+        return {
+            greeting: 'Hello'
+        }
+    },
+    components: {
+        OtherComponent
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    p {
+        font-size: 2rem;
+        text-align: center;
+    }
+</style>
---
+++ Hello/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                greeting: 'Hello'
+            }
+        }
+    };    
+</script>
+
+<style>
+p {
+    font-size: 2rem;
+    text-align: center;
+}
+</style>
