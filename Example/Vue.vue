84215347c32b7c9d510cd57451344a0311ccdb66
A baby programmer <139476654+ababyprogrammer@users.noreply.github.com>
2023-12-07 11:24:51 +0330

    Add files via upload

---
+++ Example/Vue.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" v-model="username" @input="onInput">
+        <p>{{ username }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: ''
+        }
+    },
+    methods: {
+        onInput() {
+            // perform any operations or validation on input here
+            console.log('Username updated to:', this.username);
+        }
+    }
+}
+</script>
